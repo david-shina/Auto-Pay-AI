e90098a6ab56dffd990d922fb7812700cf6b17d6
david-shina <166714706+david-shina@users.noreply.github.com>
2026-05-07 10:29:08 +0100

    Refactor imports to relative paths
    Updated import statements to use relative imports.

--- api/authRoute.py
+++ api/authRoute.py
@@ -1,13 +1,13 @@
 # api/authRoute.py
 from fastapi import APIRouter, Depends, HTTPException, logger
 from sqlmodel import Session, select
-from app.core.database import get_session
-from app.models.user import User, TelegramLinkCode
+from .core.database import get_session
+from .models.user import User, TelegramLinkCode
 from datetime import datetime, timedelta
 from pydantic import BaseModel
 import hashlib
-from app.services.payaza import create_virtual_account
-from app.models.transaction import VirtualAccount
+from .services.payaza import create_virtual_account
+from .models.transaction import VirtualAccount
 from fastapi.logger import logger
 
 router = APIRouter(prefix="/api/v1/auth", tags=["auth"])
