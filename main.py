a7e262972a4694de8966b43acfa0151c864724a4
david-shina <166714706+david-shina@users.noreply.github.com>
2026-05-07 14:25:23 +0100

    Update main.py

--- main.py
+++ main.py
@@ -69,6 +69,15 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # temporary for debugging
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 app.include_router(authRouter)
 app.include_router(webhookRouter)
