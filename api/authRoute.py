8d11e2b08a558d4dba7e821060e7dd176ef4b847
david-shina <166714706+david-shina@users.noreply.github.com>
2026-05-07 16:33:24 +0100

    Update authRoute.py

--- api/authRoute.py
+++ api/authRoute.py
@@ -110,6 +110,8 @@ def login(body: LoginRequest, session: Session = Depends(get_session)):
     if not user or not verify_password(body.password, user.hashed_password):
         raise HTTPException(status_code=401, detail="Invalid email or password")
 
+    virtualaccount = session.exec(select(VirtualAccount).where(VirtualAccount.user_id == user.id)).first()
+
     return {
     "user_id": user.id,
     "email": user.email,
